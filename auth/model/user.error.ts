41a805dfc81bbec85b576093154835114deb9469
tranngocthach0919 <113542074+tranngocthach0919@users.noreply.github.com>
2024-06-25 19:59:35 +0700

    Add files via upload

---
+++ auth/model/user.error.ts
@@ -0,0 +1,8 @@
+// domain errors: loi nghiep vu
+let ErrUserExists = new Error('User already exists');
+let ErrUserNotFound = new Error('User not found');
+
+export {
+  ErrUserExists,
+  ErrUserNotFound,
+};
